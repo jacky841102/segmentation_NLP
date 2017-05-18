e231ed764a672f3c0f28d7727b46e50a4b661be7
jacky841102 <jacky841102@gmail.com>
2017-05-18 23:51:32 +0800

    init

---
+++ models/base.py
@@ -0,0 +1,132 @@
+import tensorflow as tf
+import numpy as np
+from util import data_reader, loss
+
+class Model(object):
+    def __init__(self, args):
+        self.channel_mean = np.array([123.68, 116.779, 103.939], dtype=np.float32)
+
+        #Model parms
+        self.rnn_cells = 20
+        self.batch_size = 10
+        self.input_H = 512
+        self.input_W = 512
+        self.featmap_H = 16
+        self.featmap_W = 16
+
+        self.num_vocab = 8803
+        self.embed_dim = 1000
+        self.lstm_dim = 1000
+        self.mlp_hidden_dims = 500
+
+        #Training parms
+        self.max_iter = 18000
+        self.start_lr = 0.01
+        self.lr_decay_step = 6000
+        self.lr_decay_rate = 0.1
+        self.weight_decay = 0.0005
+        self.momentum = 0.9
+
+        #which dataset to use, e.g. referit, coco, coco+, cocoref
+        self.dataset = 'coco'
+
+        self.data_folder = './data/' + self.dataset
+        self.data_prefix = ""
+
+        self.log_folder = './log/' + self.dataset
+
+        self.snapshot = 5000
+
+    def forward(self, imcrop_batch, text_seq_batch, is_training=True):
+        raise NotImplementedError('Model must implment forward method')
+
+    def get_train_var_list(self):
+        """
+        This function returns the list of variables to train. Subclass should modify accordingly.
+        """
+        return [var for var in tf.trainable_variables()]
+
+    def build_model(self):
+        self.imcrop_batch = tf.placeholder(tf.float32, [self.batch_size, self.input_H, self.input_W, 3])
+        self.text_seq_batch = tf.placeholder(tf.int32, [self.rnn_cells, self.batch_size])
+        self.label_batch = tf.placeholder(tf.float32, [self.batch_size, self.input_H, self.input_W])
+        self.scores = self.forward(self.imcrop_batch, self.text_seq_batch)
+
+        self.train_var_list = self.get_train_var_list()
+
+        #Calculate loss
+        self.cls_loss = loss.weighted_logistic_loss(scores, self.label_batch)
+
+        # Add regularization to weight matrices (excluding bias)
+        reg_var_list = [var for var in tf.trainable_variables()
+                if (var in self.train_var_list) and
+                (var.name[-9:-2] == 'weights' or var.name[-8:-2] == 'Matrix')]
+
+        reg_loss = loss.l2_regularization_loss(reg_var_list, weight_decay)
+        self.total_loss = cls_loss + reg_loss
+
+    def train_op(self, loss, train_var_list):
+        global_step = tf.Variable(0, trainable=False)
+        learning_rate = tf.train.exponential_decay(self.start_lr, global_step, self.lr_decay_step, 
+            self.lr_decay_rate, staircase=True)
+        solver = tf.train.MomentumOptimizaer(learning_rate=learning_rate, momentum=self.momentum)
+        grads_and_vars = solver.compute_gradients(loss, var_list=self.train_var_list)
+        train_step = solver.apply_gradients(grads_and_vars, global_step=global_step)
+        return train_step
+
+    def train(self):
+        #Build model, and get train_op
+        self.build_model()
+        train_op = self.train_op(self.total_loss, self.get_train_var_list())
+
+        reader = data_reader.DataReader(self.data_folder, self.data_prefix)
+
+        cls_loss_avg = 0
+        avg_accuracy_all, avg_accuracy_pos, avg_accuracy_neg = 0, 0, 0
+        decay = 0.99
+
+        # tf.train.Saver is used to save and load intermediate models.
+        self.saver = tf.train.Saver(max_to_keep=50, keep_checkpoint_every_n_hours=1)
+
+        for n_iter in range(self.max_iter):
+            batch = reader.read_batch()
+            text_seq_val = batch['text_seq_batch']
+            imcrop_val = batch['imcrop_batch'].astype(np.float32) - self.channel_mean
+            label_val = batch['label_fine_batch'].astype(np.float32)
+
+            # Forward and Backward pass
+            scores_val, cls_loss_val, _, lr_val = sess.run([self.scores, self.cls_loss, train_op, self.learning_rate],
+                feed_dict={
+                    self.text_seq_batch  : text_seq_val,
+                    self.imcrop_batch    : imcrop_val,
+                    self.label_batch     : label_val
+                })
+            cls_loss_avg = decay*cls_loss_avg + (1-decay)*cls_loss_val
+            print('\titer = %d, cls_loss (cur) = %f, cls_loss (avg) = %f, lr = %f'
+                % (n_iter, cls_loss_val, cls_loss_avg, lr_val))
+
+            # Accuracy
+            accuracy_all, accuracy_pos, accuracy_neg = segmodel.compute_accuracy(scores_val, label_val)
+            avg_accuracy_all = decay*avg_accuracy_all + (1-decay)*accuracy_all
+            avg_accuracy_pos = decay*avg_accuracy_pos + (1-decay)*accuracy_pos
+            avg_accuracy_neg = decay*avg_accuracy_neg + (1-decay)*accuracy_neg
+            print('\titer = %d, accuracy (cur) = %f (all), %f (pos), %f (neg)'
+                % (n_iter, accuracy_all, accuracy_pos, accuracy_neg))
+            print('\titer = %d, accuracy (avg) = %f (all), %f (pos), %f (neg)'
+                % (n_iter, avg_accuracy_all, avg_accuracy_pos, avg_accuracy_neg))
+
+            if (n_iter + 1) % self.snapshot == 0 or (n_iter+1) >= self.max_iter:
+                checkpoint_path = os.path.join(self.log_folder, 'checkpoints')
+                self.save(checkpoint_path, n_iter)
+
+    def load(self, checkpoint_dir='checkpoints', step=None):
+        pass
+
+    def save(self, checkpoint_dir, step):
+        model_name = "Scene.model"
+        if not os.path.exists(checkpoint_dir):
+            os.makedirs(checkpoint_dir)
+        self.saver.save(self.sess, os.path.join(checkpoint_dir, model_name), global_step=step)
+
+    def test(self, data_list):
+        pass
---
+++ util/rnn.py
@@ -0,0 +1,117 @@
+from __future__ import absolute_import, division, print_function
+
+import tensorflow as tf
+from util.ConvLSTMCell import BasicConvLSTMCell
+
+def lstm_layer(name, seq_bottom, const_bottom, output_dim, num_layers=1,
+               forget_bias=0.0, apply_dropout=False, keep_prob=0.5,
+               concat_output=True):
+    """
+    Similar LSTM layer as the `LSTMLayer` in Caffe
+    ----
+    Args:
+        seq_bottom : the underlying sequence input of size [T, N, D_in], where
+            D_in is the input dimension, T is num_steps and N is batch_size.
+        const_bottom : the constant bottom concatenated to each time step,
+            having shape [N, D_const]. This can be *None*. If it is None,
+            then this input is ignored.
+        output_dim : the number of hidden units in the LSTM unit and also the
+            final output dimension, i.e. D_out.
+        num_layers : the number of stacked LSTM layers.
+        forget_bias : forget gate bias in LSTM unit.
+        apply_dropout, keep_prob: dropout applied to the output of each LSTM
+            unit.
+    Returns:
+        output : a list of [T, N, D_out], where D_out is output_dim,
+            T is num_steps and N is batch_size
+    """
+
+    # input shape is [T, N, D_in]
+    input_shape = seq_bottom.get_shape().as_list()
+    # the number of time steps to unroll
+    num_steps = input_shape[0]
+    # batch size (i.e. N)
+    batch_size = input_shape[1]
+
+    # The actual parameter variable names are as follows (`name` is the name
+    # variable here, and Cell0, Cell1, ... are num_layers stacked LSTM cells):
+    #   `name`/RNN/MultiRNNCell/Cell0/BasicLSTMCell/Linear/Matrix
+    #   `name`/RNN/MultiRNNCell/Cell0/BasicLSTMCell/Linear/Bias
+    #   `name`/RNN/MultiRNNCell/Cell1/BasicLSTMCell/Linear/Matrix
+    #   `name`/RNN/MultiRNNCell/Cell1/BasicLSTMCell/Linear/Bias
+    # where Cell1 is on top of Cell0, taking Cell0's hidden states as inputs.
+    #
+    # For Cell0, the weight matrix ('BasicLSTMCell/Linear/Matrix') has shape
+    # [D_in+D_const+D_out, 4*D_out], and bias has shape [4*D_out].
+    # For Cell1, Cell2, ..., the weight matrix ('BasicLSTMCell/Linear/Matrix')
+    # has shape [D_out*2, 4*D_out], and bias has shape [4*D_out].
+    # In the weight matrix, the first D_in+D_const rows (in Cell0) or D_out rows
+    # (in Cell1, Cell2, ...) are bottom input weights, and the rest D_out rows
+    # are state weights, i.e. *inputs are before states in weight matrix*
+    #
+    # The gate order in 4*D_out are i, j, f, o, where
+    # i = input_gate, j = new_input, f = forget_gate, o = output_gate
+    #
+    # Other details in tensorflow/python/ops/rnn_cell.py
+    with tf.variable_scope(name):
+        # the basic LSTM cell
+        lstm_cell = tf.contrib.rnn.BasicLSTMCell(output_dim, forget_bias)
+        # Apply dropout if specified.
+        if apply_dropout and keep_prob < 1:
+            lstm_cell = tf.contrib.rnn.DropoutWrapper(
+                lstm_cell, output_keep_prob=keep_prob)
+        cell = tf.contrib.rnn.MultiRNNCell([lstm_cell] * num_layers)
+
+        # Initialize cell state from zero.
+        initial_state = cell.zero_state(batch_size, tf.float32)
+        # Fix batch_size issue when batch_size == 1
+        # state_shape = initial_state.get_shape().as_list()
+        # state_shape[0] = batch_size
+        # initial_state.set_shape(state_shape)
+
+        # Split along time dimension and flatten each component.
+        # `inputs` is a list.
+        inputs = [tf.reshape(input_, [batch_size, -1])
+            for input_ in tf.split(axis=0, num_or_size_splits=num_steps, value=seq_bottom)]
+        # Add constant input to each time step.
+        if not const_bottom is None:
+            # Flatten const_bottom into shape [N, D_const] and concatenate.
+            const_input_ = tf.reshape(const_bottom, [batch_size, -1])
+            inputs = [tf.concat(axis=0, values=[input_, const_input_])
+                for input_ in inputs]
+
+        # Create the Recurrent Network and collect `outputs`. `states` are
+        # ignored.
+        outputs, _ = tf.contrib.rnn.static_rnn(cell, inputs, initial_state=initial_state)
+        if concat_output:
+            # Concat the outputs into [T, N, D_out].
+            outputs = tf.reshape(tf.concat(axis=0, values=outputs),
+                                [num_steps, batch_size, output_dim])
+    return outputs
+
+def mlstm_layer(name, seq_bottom, const_bottom, output_dim, num_layers=1,
+               forget_bias=0.0, apply_dropout=False, keep_prob=0.5,
+               concat_output=True):
+    
+    batch_size, num_steps, H, W, channel = seq_bottom.get_shape().as_list()
+
+    # input_list = tf.split(0, num_steps, seq_bottom)
+    # input_list = [tf.squeeze(p_input_, [0]) for input_ in input_list]
+    with tf.variable_scope(name):
+        # cell = ConvLSTMCell(output_dim)
+        cell = BasicConvLSTMCell((H, W), (1, 1), output_dim)
+        # cell = tf.contrib.rnn.MultiRNNCell([mlstm_cell] * num_layers)
+        new_state = cell.zero_state(batch_size, tf.float32)
+
+        inputs = tf.transpose(seq_bottom, [1, 0, 2, 3, 4])
+        
+        outputs = []
+        for i in range(num_steps):
+            output, new_state = cell(inputs[i], new_state, name)
+            if i == 0:
+                tf.get_variable_scope().reuse_variables()
+            outputs.append(output)
+
+    return outputs[-1], outputs
+
+    
